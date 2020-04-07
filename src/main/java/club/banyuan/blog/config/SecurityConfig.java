37505e9b4411aee743f47485875d69e857fe86e1
linchuan <linc1982@icloud.com>
2020-04-07 17:46:59 +0800

    update mysql username

--- src/main/java/club/banyuan/blog/config/SecurityConfig.java
+++ src/main/java/club/banyuan/blog/config/SecurityConfig.java
@@ -29,9 +29,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.
-                csrf().disable().
-                authorizeRequests()
+        http
+            .csrf().disable()
+                .authorizeRequests()
                 .antMatchers("/admin/**").authenticated()
                 .anyRequest().permitAll()
             .and()
