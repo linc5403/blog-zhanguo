9a9d25d0113a2cbbbf14ab399aa0a9c8e9147740
lin chuan <linch1982@gmail.com>
2020-04-07 22:33:22 +0800

    add redis support for session

--- src/main/java/club/banyuan/blog/bean/User.java
+++ src/main/java/club/banyuan/blog/bean/User.java
@@ -2,8 +2,10 @@ package club.banyuan.blog.bean;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
-public class User {
+public class User implements Serializable{
     Integer id;
     String name;
     String password;
